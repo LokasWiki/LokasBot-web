ab9a275fc00c0911506f66dbedd6b050d92fbfa3
lokas <lokashafeek7755@gmail.com>
2023-01-25 13:23:20 +0200

    update cdn link

--- bin/setup-venvs.sh
+++ bin/setup-venvs.sh
@@ -9,10 +9,8 @@ function setup-venv {
     python3 -m venv "$HOME/www/python/$1"
     . "$HOME/www/python/$1/bin/activate"
     python3 -m pip install -U pip setuptools wheel
-    python -m pip install "requests>=2.20.1"
-    python -m pip install "mwparserfromhell>=0.5.0"
-    python -m pip install "wikitextparser>=0.47.5"
-    python -m pip install pywikibot
+    python -m pip install requests
+    python -m pip install pyyaml
     # shellcheck disable=SC2086
     python3 -m pip install $2
     deactivate
