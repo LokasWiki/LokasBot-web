a35dc9b5368e03c877fc193c0f76e6c1c2a03850
lokas <lokashafeek7755@gmail.com>
2023-04-05 08:01:46 +0200

    add pages

--- app/tasks.py
+++ app/tasks.py
@@ -1,5 +1,3 @@
-from markupsafe import escape
-
 from flask import (
     Blueprint, render_template, url_for, abort
 )
@@ -19,7 +17,7 @@ def task_index(name):
     if name == "webcite":
         taskname = TaskName.WEBCITE
 
-    total_count = db_session.query(Page).count()
+    total_count = db_session.query(Page).filter_by(task_name=taskname).count()
     status_0_count = db_session.query(Page).filter_by(status=Status.PENDING, task_name=taskname).count()
     status_1_count = db_session.query(Page).filter_by(status=Status.RECEIVED, task_name=taskname).count()
     pages_list = db_session.query(Page).filter_by(status=Status.RECEIVED, task_name=taskname).order_by(
@@ -27,4 +25,11 @@ def task_index(name):
 
     return render_template("tasks/show.html", total_count=total_count, status_0_count=status_0_count,
                            status_1_count=status_1_count, pages_list=pages_list,
-                           all_url=url_for('tasks.task_index', name=name))
+                           all_url=url_for('tasks.list_of_pages_in_tasks', name=name))
+
+
+@bp.route("/show/<name>/pages")
+def list_of_pages_in_tasks(name):
+    if escape(name) not in ["maintenance", "webcite"]:
+        abort(404)
+    return render_template("tasks/pages.html", name=name)
